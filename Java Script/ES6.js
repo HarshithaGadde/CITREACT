e809115cbc7a4932633364852a780751c7735823
HarshithaGadde <91651852+HarshithaGadde@users.noreply.github.com>
2022-03-20 13:58:04 +0530

    Add files via upload

---
+++ Java Script/ES6.js
@@ -0,0 +1,5 @@
+// ES6 features:
+      
+        // -spread operator(...)
+let arr1=[1,2,3,4];
+let arr2=[-1,0,arr1];
---
+++ Java Script/rest parameter.js
@@ -0,0 +1,6 @@
+function demo(a,b,....restlist)
+{
+    console.log(a,b,)
+    console.log(...a.restlist)
+}
+ demo(2,3,4,5,55,765,789,56789,87356)
